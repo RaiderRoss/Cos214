3f034ef3cbe4b18d907128ad3646725d06f44635
RaiderRoss <aidan1.mck@gmail.com>
2024-08-11 20:25:26 +0200

    Fixed Care taker kinda

--- Prac2/Soldiers.cpp
+++ Prac2/Soldiers.cpp
@@ -1,27 +1,24 @@
-//
-// Created by 2MGOU on 2024/07/30.
-//
-
 #include "Soldiers.h"
+Caretaker Soldiers::caretaker;
 
 Memento* Soldiers::militusMemento() {
-  return new Memento(healthPerSoldier,damagePerSoldier,defencePerSoldifer,amountOfSoldiersPerUnit,unitName);
+    return new Memento(healthPerSoldier, damagePerSoldier, defencePerSoldifer, amountOfSoldiersPerUnit, unitName);
 }
 
 void Soldiers::vivificaMemento(Memento* mem) {
-  this->unitName = mem->getName();
-  this->healthPerSoldier = mem->getHealth();
-  this->damagePerSoldier = mem->getDamage();
-  this->defencePerSoldifer = mem->getDefence();
-  this->amountOfSoldiersPerUnit = mem->getAmount();
+    this->unitName = mem->getName();
+    this->healthPerSoldier = mem->getHealth();
+    this->damagePerSoldier = mem->getDamage();
+    this->defencePerSoldifer = mem->getDefence();
+    this->amountOfSoldiersPerUnit = mem->getAmount();
 }
 
 void Soldiers::engage() {
-  prepare();
-  execute();
+    prepare();
+    execute();
 }
 
 void Soldiers::disengage() {
-  retreat();
-  rest();
+    retreat();
+    rest();
 }
--- Prac2/Soldiers.h
+++ Prac2/Soldiers.h
@@ -1,37 +1,35 @@
-//
-// Created by 2MGOU on 2024/07/30.
-//
-
 #ifndef SOLDIERS_H
 #define SOLDIERS_H
+
 #include <iostream>
 #include <string>
-using namespace std;
 #include "Memento.h"
 #include "Caretaker.h"
+using namespace std;
 class Soldiers {
  private:
-  int healthPerSoldier;
-  int damagePerSoldier;
-  int defencePerSoldifer;
-  int amountOfSoldiersPerUnit;
-  std::string unitName;
-  virtual void prepare() = 0;
-  virtual void execute() = 0;
-  virtual void retreat() = 0;
-  virtual void rest() = 0;
-  static Caretaker caretaker = new;
+    int healthPerSoldier;
+    int damagePerSoldier;
+    int defencePerSoldifer;
+    int amountOfSoldiersPerUnit;
+    std::string unitName;
+    virtual void prepare() = 0;
+    virtual void execute() = 0;
+    virtual void retreat() = 0;
+    virtual void rest() = 0;
+    static Caretaker caretaker;  
 
  public:
-  void engage();
-  void disengage();
-  Memento* militusMemento();
-  void vivificaMemento(Memento* mem);
-  virtual int getHealthPerSoldier() = 0;
-  virtual int getDamagePerSoldier() = 0;
-  virtual int getDefencePerSoldier() = 0;
-  virtual int getAmountOfSoldiersPerUnit() = 0;
-  virtual Soldiers* clonis() = 0;
+    void engage();
+    void disengage();
+    Memento* militusMemento();
+    void vivificaMemento(Memento* mem);
+    
+    virtual int getHealthPerSoldier() = 0;
+    virtual int getDamagePerSoldier() = 0;
+    virtual int getDefencePerSoldier() = 0;
+    virtual int getAmountOfSoldiersPerUnit() = 0;
+    virtual Soldiers* clonis() = 0;
 };
 
 #endif  // SOLDIERS_H
