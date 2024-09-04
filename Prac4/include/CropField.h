1ec1671ec59e882ca6393267f0df07b06928a75e
CartographySilence <u23668882@tuks.co.za>
2024-09-04 18:44:47 +0200

    Worked on CropField

--- Prac4/include/CropField.h
+++ Prac4/include/CropField.h
@@ -6,13 +6,17 @@
 #include "Soil.h"
 class  CropField : public FarmUnit {
 public:
-    CropField (std::string crop, int capacity, Soil soil);
+    CropField (std::string crop, int capacity, Soil* soil);
     ~CropField ();
     int getTotalCapacity();
     void plantCrops(int planted);
+    std::string getCropType();
+    std::string getSoilState();
 private:
     std::string cropType;
-    Soil soil;
+    Soil* soil;
+    int capacity;
+    int planted;
 };
 
 #endif //  CROPFIELD_H
--- Prac4/src/CropField.cpp
+++ Prac4/src/CropField.cpp
@@ -1,10 +1,31 @@
 #include "CropField.h"
 
-CropField :: CropField (std::string crop, int capacity, Soil soil) {
-    // Constructor implementation
+CropField :: CropField (std::string crop, int capacity, Soil* soil) {
+    this->cropType = crop;
+    this->capacity = capacity;
+    this->soil = soil;
+    this->planted = 0;
 }
 
 CropField ::~ CropField () {
-    // Destructor implementation
+    delete this;
 }
 
+int CropField::getTotalCapacity() {
+    return this->capacity;
+}
+
+void CropField::plantCrops(int planted) {
+    if(this->planted + planted < this->capacity ) {
+        this->planted += planted;
+    }
+    return;
+}
+
+std::string CropField::getCropType() {
+    return this->cropType;
+}
+
+std::string CropField::getSoilState() {
+    return this->soil->getName();
+}
